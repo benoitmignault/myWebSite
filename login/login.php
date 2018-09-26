bc9cebb80e1cf7f5b227baa5963413fcf4762a31
Benoit Mignault <b.mignault@gmail.com>
2018-09-25 20:48:15 -0400

    Debut du sous projet statistiques des connexions à la page des statistiques du poker. Inversion des connexions à la BD Dev au lieu de la prod

--- login/login.php
+++ login/login.php
@@ -194,16 +194,18 @@ function connexionUser($champInitial, $connMYSQL) {
 }
 
 function connexionBD() {
+    /*
     $host = "benoitmignault.ca.mysql";
     $user = "benoitmignault_ca_mywebsite";
     $password = "d-&47mK!9hjGC4L-";
     $bd = "benoitmignault_ca_mywebsite";
-    /*
+    */
+    
     $host = "localhost";
     $user = "zmignaub";
     $password = "Banane11";
     $bd = "benoitmignault_ca_mywebsite";
-    */
+    
     $connMYSQL = mysqli_connect($host, $user, $password, $bd);
     $connMYSQL->query("set names 'utf8'");
     return $connMYSQL;

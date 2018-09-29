2e6774665a59e5d86b7f71850a0a0f5e2c679939
Benoit Mignault <b.mignault@gmail.com>
2018-09-29 03:17:36 -0400

    encore des petits ajustements mais en fonction de la largeur de la fenêtre aussi avant de vérifier le zoom pixels

--- timer/timer.js
+++ timer/timer.js
@@ -3,6 +3,7 @@ const timerReprend = document.querySelector('.resizeText');
 const temps_periode = document.querySelector('.container .timer .tableauDuTemps .temps div .resizeText');
 const type_mises = document.querySelector('.container .timer .tableauDesMises .lesMises div .resizeText');
 const btn_return = document.querySelector('.boutonRetour .retour form .resizeText');
+const btn_choisir = document.querySelector('.container .tableau_bord form .choix .bouton');
 const resetValeur = document.querySelector('#reset');
 const typeLangue = document.querySelector('#typeLangue');
 const temps15min = document.querySelector('#timer15');
@@ -314,26 +315,51 @@ function resetTemp(){
 
 function resizeText(){
     var device = detectZoom.device();
-    if (device > 1.6){
-        reprendTimer.style.fontSize = "20px";
-        temps_periode.style.fontSize = "30px";
-        type_mises.style.fontSize = "30px";
-        btn_return.style.fontSize = "20px";
-    } else if (device > 1.3){
-        reprendTimer.style.fontSize = "30px";
-        temps_periode.style.fontSize = "35px";
-        type_mises.style.fontSize = "35px";
-        btn_return.style.fontSize = "30px";
-    } else if (device > 1.1){
-        reprendTimer.style.fontSize = "35px";
-        temps_periode.style.fontSize = "40px";
-        type_mises.style.fontSize = "40px";
-        btn_return.style.fontSize = "35px";
+    var largeur = window.innerWidth;
+    if (largeur < 1300){ 
+        if (device > 1.2){
+            reprendTimer.style.fontSize = "20px";
+            temps_periode.style.fontSize = "30px";
+            type_mises.style.fontSize = "30px";
+            btn_return.style.fontSize = "20px";
+            btn_choisir.style.fontSize = "20px";
+        } else if (device >= 1.1){
+            reprendTimer.style.fontSize = "25px";
+            temps_periode.style.fontSize = "35px";
+            type_mises.style.fontSize = "35px";
+            btn_return.style.fontSize = "25px";
+            btn_choisir.style.fontSize = "25px";
+        } else if (device >= 1){
+            reprendTimer.style.fontSize = "30px";
+            temps_periode.style.fontSize = "40px";
+            type_mises.style.fontSize = "40px";
+            btn_return.style.fontSize = "30px";
+            btn_choisir.style.fontSize = "30px";
+        }
+    } else if (largeur > 1600){ 
+        if (device > 1.6){
+            reprendTimer.style.fontSize = "20px";
+            temps_periode.style.fontSize = "30px";
+            type_mises.style.fontSize = "30px";
+            btn_return.style.fontSize = "20px";
+            btn_choisir.style.fontSize = "20px";
+        } else if (device > 1.3){
+            reprendTimer.style.fontSize = "30px";
+            temps_periode.style.fontSize = "35px";
+            type_mises.style.fontSize = "35px";
+            btn_return.style.fontSize = "30px";
+            btn_choisir.style.fontSize = "30px";
+        } else if (device > 1.1){
+            reprendTimer.style.fontSize = "35px";
+            temps_periode.style.fontSize = "40px";
+            type_mises.style.fontSize = "40px";
+            btn_return.style.fontSize = "35px";
+            btn_choisir.style.fontSize = "35px";
+        }
     }
     // 1.1041666269302368 -> 100%
     // 1.2145832777023315 -> 110%
     // 1.3802082538604736 -> 125%
-    console.log(device);
 } 
 
 document.addEventListener('DOMContentLoaded', function(event) {   

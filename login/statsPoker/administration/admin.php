bc42598d3f8b434a03c5c6a4d88902fe80716e0a
frantz256255-PC\frantz256255 <b.mignault@gmail.com>
2019-03-13 19:00:44 -0400

    Ajout HTTPS pour le script javascript

--- login/statsPoker/administration/admin.php
+++ login/statsPoker/administration/admin.php
@@ -753,7 +753,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </div>
             </form> 
         </div>
-        <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
         <script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
         <script type="text/javascript" src="date.js"></script>
     </body>

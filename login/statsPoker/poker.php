65542f95ca43cde7864aaede11ef2d6c65eec2ca
frantz256255-PC\frantz256255 <b.mignault@gmail.com>
2019-08-13 19:52:48 -0400

    Remise en service des BD local pour le refactoring de login, et reset

--- login/statsPoker/poker.php
+++ login/statsPoker/poker.php
@@ -95,7 +95,7 @@ function remplissage_Champs($array_Champs){
         if (isset($_GET['method'])){
             $array_Champs['method'] = intval($_GET['method']);            
         }
-        
+
         if ($array_Champs['method'] == 4){
             $array_Champs['nombre_Presences'] = intval($_GET['nombre_Presences']);
         }
@@ -655,18 +655,19 @@ function delete_Session(){
 
 function connexionBD() { 
     // Nouvelle connexion sur hébergement du Studio OL
-    
+
+    /*
     $host = "localhost";
     $user = "benoitmi_benoit";
     $password = "d-&47mK!9hjGC4L-";
     $bd = "benoitmi_benoitmignault.ca.mysql";
-    /*
+    */
 
     $host = "localhost";
     $user = "zmignaub";
     $password = "Banane11";
     $bd = "benoitmignault_ca_mywebsite";
-*/
+
     $connMYSQL = mysqli_connect($host, $user, $password, $bd);
     $connMYSQL->query("set names 'utf8'");
     return $connMYSQL;
@@ -749,7 +750,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $liste_Joueur_method6 = creationListeDate($connMYSQL, $arrayMots['option'], $array_Champs['tournoiDate']);
         }
     }  
-    
+
     $connMYSQL->close();
 } // fin du GET
 

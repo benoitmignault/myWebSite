61b23f57c55c589632d23bdb175c924297627a5f
Benoit Mignault <b.mignault@gmail.com>
2022-12-18 20:29:21 -0500

    Création et insertion du fichier fct_connexion_bd

---
+++ fonction_commune/fct_connexion_bd.php
@@ -0,0 +1,21 @@
+<?php
+	function connexionBD() {
+		// Nouvelle connexion sur hébergement du Studio OL
+		$host = "localhost";
+		$user = "benoitmi_benoit";
+		$password = "d-&47mK!9hjGC4L-";
+		$bd = "benoitmi_benoitmignault.ca.mysql";
+		
+		$connMYSQL = mysqli_connect($host, $user, $password, $bd);
+		$connMYSQL->query("set names 'utf8'");
+		
+		// Vérification de la connexion
+		if ($connMYSQL->connect_error) {
+			die('Erreur de connexion (' . $connMYSQL->connect_errno . ') ' . $connMYSQL->connect_error);
+		}
+		else {
+			return $connMYSQL;
+		}
+	}
+
+?>

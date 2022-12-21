b0f50129c80fa73be7f89ff5298944b30e5ed106
Benoit Mignault <b.mignault@gmail.com>
2022-12-20 23:16:50 -0500

    Création de la mécanique de récupération du fichier Json

--- timer/timer.php
+++ timer/timer.php
@@ -22,19 +22,49 @@
 		        'aucuneValeurDispo' => false, 'aucunOrganisateur' => false, 'erreurPossible' => false];
 	}
 	
- 
- 
-	function traduction($typeLangue): array {
+	/**
+	 * Retourne une structure de fichier Json qui contient le dictionnaire
+	 * @param $chemin
+	 * @return mixed
+	 */
+	function recuperationContenuFichierJson($chemin) {
 		
-	
+		// Décoder les données JSON et les stocker dans un tableau associatif
+		return json_decode(file_get_contents($chemin), true);
 	}
 	
+	/**
+     * Retourne soit le mot traduit ou le mot lui-même, si l'utilisateur est sur la page en français
+	 * @param $motATraduire
+	 * @param $dictionnaire
+	 * @return string
+	 */
+	function traduction($motATraduire, $dictionnaire): string {
+		
+		$mot = "";
+  
+		if (array_key_exists($motATraduire, $dictionnaire)) {
+			// Retourner la traduction du mot, s'il doit être traduit
+			if ($_SERVER['typeLangue'] === "english") {
+				$mot = $dictionnaire[$motATraduire];
+			}
+            elseif ($_SERVER['typeLangue'] === "francais") {
+				$mot = $motATraduire;
+			}
+		}
+		else {
+			// Retourner un message d'erreur si le mot n'est pas présent dans le dictionnaire
+			if ($_SERVER['typeLangue'] === "english") {
+				$mot = "Word not found in dictionary.";
+			}
+            elseif ($_SERVER['typeLangue'] === "francais") {
+				$mot = "Mot introuvable dans le dictionnaire.";
+			}
+		}
+		
+		return $mot;
+	}
 	
-    
-    
-    
-    
-    
 	
 	/**
 	 * Retourne le message en fonction de la langue et de la situation séparé en deux fonctions différentes.

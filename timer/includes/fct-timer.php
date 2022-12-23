10036abd5d3e17239ba008dd3fdc11639524bff6
Benoit Mignault <b.mignault@gmail.com>
2022-12-22 20:30:54 -0500

    Récupération de la fct traduction

--- timer/includes/fct-timer.php
+++ timer/includes/fct-timer.php
@@ -2,7 +2,6 @@
 	
 	/**
 	 * Retour la prochaine sélection de small & big blind
-	 * Cette function est utilisé par timer.php & changer.php qui est appelé à partir d'un call Ajax de timer.js
 	 * @param $connMYSQL
 	 * @param $nomOrganisateur
 	 * @param $combinaison
@@ -97,5 +96,36 @@
 		
 		return $couleurs;
 	}
-
-?>
+	
+	/**
+	 * Retourne soit le mot traduit ou le mot lui-même, si l'utilisateur est sur la page en français
+	 * @param $motATraduire
+	 * @param $dictionnaire
+	 * @return string
+	 */
+	function traduction($motATraduire, $dictionnaire): string {
+		
+		$mot = "";
+		
+		if (array_key_exists($motATraduire, $dictionnaire)) {
+			// Retourner la traduction du mot, s'il doit être traduit
+			if ($_SERVER['typeLangue'] === "english") {
+				$mot = $dictionnaire[$motATraduire];
+			}
+			elseif ($_SERVER['typeLangue'] === "francais") {
+				$mot = $motATraduire;
+			}
+		}
+		else {
+			// Retourner un message d'erreur si le mot n'est pas présent dans le dictionnaire
+			if ($_SERVER['typeLangue'] === "english") {
+				$mot = "Word not found";
+			}
+			elseif ($_SERVER['typeLangue'] === "francais") {
+				$mot = "Mot introuvable";
+			}
+		}
+		
+		return $mot;
+	}
+	

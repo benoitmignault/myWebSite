cf2b346f0aeb9997674d1ad0135768af03279b5a
Benoit Mignault <b.mignault@gmail.com>
2023-12-26 22:34:14 -0500

    Déplacement des fct de traductions vers le fichier associé

--- login/createLinkSendMail.php
+++ login/createLinkSendMail.php
@@ -1,14 +1,17 @@
 <?php
-
-// Import PHPMailer classes into the global namespace
+    // Les includes nécessaires
+    include("../traduction/traduction_create_link_reset.php");
+	include_once("../includes/fct-connexion-bd.php");
+    
+    // Import PHPMailer classes into the global namespace
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
 
-// Load Composer's autoloader
+    // Load Composer's autoloader
     // require '../../vendor/autoload.php';
 
-// Lorsque je suis en mode DEV :
+    // Lorsque je suis en mode DEV :
 	require '../PHPMailer/src/Exception.php';
 	require '../PHPMailer/src/PHPMailer.php';
 	require '../PHPMailer/src/SMTP.php';
@@ -463,93 +466,6 @@ function redirection(string $type_langue) {
     exit; // pour arrêter l'exécution du code php
 }
 
-/**
- * Fonction qui sera utiliser pour traduire le texte dans la page et ainsi que les messages d'erreurs
- *
- * @param string $type_langue
- * @param int $situation
- * @return string[]
- */
-function traduction_liste_mots(string $type_langue, int $situation): array {
-    
-    // Initialiser le array de mots traduit
-    $liste_mots = array("lang" => "", 'message' => "", 'title' => "", 'p1' => "", 'li3' => "", 'li1' => "",
-                        'li2' => "", 'legend' => "", 'email' => "", 'btn_envoi_lien' => "", 'btn_return' => "");
-    
-    if ($type_langue === 'francais') {
-        $liste_mots["lang"] = "fr";
-        $liste_mots["title"] = "Demande de Réinitialisation";
-        $liste_mots["p1"] = "Vous avez oublié votre mot de passe, pas de problème, on s'en occupe !";
-        $liste_mots["li3"] = "Cette page permet de réinitialiser votre compte associés aux statistiques de poker.";
-        $liste_mots["li1"] = "Veuillez saisir votre courriel.";
-        $liste_mots["li2"] = "Ensuite, un courriel vous sera envoyé avec toute les informations relier à votre changement de mot de passe.";
-        $liste_mots["legend"] = "Réinitialisation !";
-        $liste_mots["email"] = "Courriel :";
-        $liste_mots["btn_envoi_lien"] = "Réinitialiser";
-        $liste_mots["btn_return"] = "Retour à l'accueil";
-        
-    } elseif ($type_langue === 'english') {
-        $liste_mots["title"] = "Reset Request";
-        $liste_mots["lang"] = "en";
-        $liste_mots["p1"] = "You forgot your password, no problem, we take care of it !";
-        $liste_mots["li3"] = "This page will reset your account associated with poker statistics.";
-        $liste_mots["li1"] = "Please enter your email.";
-        $liste_mots["li2"] = "Then, a mail will be sent to you with all the information related to your change of password.";
-        $liste_mots["legend"] = "Reseting !";
-        $liste_mots["email"] = "Email :";
-        $liste_mots["btn_envoi_lien"] = "Reset";
-        $liste_mots["btn_return"] = "Return to home page";
-    }
-    
-    // Le message qui sera dans la langue voulu
-    $liste_mots["message"] = traduction_situation($type_langue, $situation);
-    
-    return $liste_mots;
-}
-
-/**
- * @param string $type_langue
- * @param int $situation
- * @return string
- */
-function traduction_situation(string $type_langue, int $situation): string{
-    
-    $message = "";
-    if ($type_langue === 'francais') {
-        
-        switch ($situation) {
-            case 1 : $message = "Le champ «Courriel» est vide !"; break;
-            case 2 : $message = "Le courriel saisie est trop long pour l'espace disponible !"; break;
-            case 3 : $message = "Le courriel saisie ne respecte pas la forme « exemple@email.com »"; break;
-            case 4 : $message = "Le courriel saisie n'existe pas dans nos informations !"; break;
-            case 5 : $message = "Une erreur de communication/manipulation est survenu au moment de vous envoyer le courriel !"; break;
-            case 6 : $message = "Dans les prochains instant, vous allez recevoir le courriel de réinitialisation avec toutes les informations nécessaire !"; break;
-            case 7 : $message = "Erreur Système au moment d'envoyer le courriel !"; break;
-            case 8 : $message = "Vous avez déjà reçu un courriel pour changer votre mot de passe, il n'est pas nécessaire de faire une nouvelle demande !"; break;
-        }
-        
-    } elseif ($type_langue === 'english') {
-        
-        switch ($situation) {
-            case 1 : $message = "The «Email» field is empty !"; break;
-            case 2 : $message = "The seized mail is too long for the available space !"; break;
-            case 3 : $message = "The seized mail does not follow the form « example@email.com » !"; break;
-            case 4 : $message = "The entered email does not exist in our information !"; break;
-            case 5 : $message = "A communication / manipulation error occurred when sending you the email !"; break;
-            case 6 : $message = "In the next few moments, you will receive the reset email with all the necessary information !"; break;
-            case 7 : $message = "System error when sending email !"; break;
-            case 8 : $message = "You have already received an email to change your password, there is no need to make a new request !"; break;
-        }
-    }
-    
-    return $message;
-}
-
-
-
-
-include_once("../includes/fct-connexion-bd.php");
-
 // Les fonctions communes
     $connMYSQL = connexion();
     $array_Champs = initialisation();
@@ -564,7 +480,7 @@ include_once("../includes/fct-connexion-bd.php");
             redirection(""); // On n'a pas besoin de la variable vue qu'elle ne ressemble à rien de connue
         } else {
             // La variable de situation est encore à 0 vue qu'il s'est rien passé de grave...
-            $array_Champs["liste_mots"] = traduction_liste_mots($array_Champs["type_langue"], $array_Champs["situation"]);
+            $array_Champs["liste_mots"] = traduction($array_Champs["type_langue"], $array_Champs["situation"]);
         }
     } // Fin du GET pour faire afficher la page web
 
@@ -573,20 +489,17 @@ include_once("../includes/fct-connexion-bd.php");
     
         if (isset($_POST['btn_return']))  {
             redirection($array_Champs["type_langue"]);
-        } else {
             
-            // Si le bouton se connecter est pesé...
-            if (isset($_POST['btn_envoi_lien'])) {
-                
-                $champs = validation_champs($array_Champs);
-                if (!$array_Champs['erreur_presente']){
-                    // $array_Champs = gestion_lien_courriel($array_Champs, $connMYSQL);
-                    // $champs = creationLink($champs, $connMYSQL);
-                }
+        } elseif (isset($_POST['btn_envoi_lien'])) {
+	
+	        $array_Champs = validation_champs($array_Champs);
+            if (!$array_Champs['erreur_presente']){
+                // $array_Champs = gestion_lien_courriel($array_Champs, $connMYSQL);
+                // $champs = creationLink($champs, $connMYSQL);
             }
         
             $array_Champs["situation"] = situation($array_Champs);
-            $array_Champs["liste_mots"] = traduction_liste_mots($array_Champs["type_langue"], $array_Champs["situation"]);
+            $array_Champs["liste_mots"] = traduction($array_Champs["type_langue"], $array_Champs["situation"]);
          
             $connMYSQL->close();
         }

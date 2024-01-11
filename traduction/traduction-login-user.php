90f148cc26385ad90ccd75ad8256ce0d9f23d980
Benoit Mignault <b.mignault@gmail.com>
2024-01-10 23:46:21 -0500

    Ajustement dans la page de traduction

--- traduction/traduction-login-user.php
+++ traduction/traduction-login-user.php
@@ -9,9 +9,9 @@
 	 */
 	function traduction(string $type_langue, int $situation): array {
 		
-		$liste_mots = array("lang" => "", 'emailInfo' => "", 'title' => "", 'email' => "", 'p1' => "", 'li1' => "",
-		                     'li2' => "", 'li3' => "", 'legend' => "", 'usager' => "", 'mdp' => "", 'btn_login' => "",
-		                     "btn_effacer" => "", 'btn_signUp' => "", 'btn_reset' => "", 'btn_return' => "", 'message' => "");
+		$liste_mots = array("lang" => "", 'title' => "", 'p1' => "", 'li1' => "",
+		                     'li2' => "", 'legend' => "", 'usager' => "", 'pwd' => "", 'btn_login' => "",
+		                     "btn_reset" => "", 'btn_sign_up' => "", 'btn_reset_pwd' => "", 'btn_return' => "", 'message' => "");
 		
 		if ($type_langue === 'francais') {
 			$liste_mots["lang"] = "fr";
@@ -19,14 +19,13 @@
 			$liste_mots["p1"] = "Bienvenue à la page de connexion des statistiques du poker entre amis !";
 			$liste_mots["li1"] = "Vous devez vous authentifier, pour faire afficher les statistiques désirées";
 			$liste_mots["li2"] = "Si vous n'avez pas de compte, veuillez vous en créer un.";
-			$liste_mots["li3"] = "Veuillez spécifier un nom d'utilisateur et un courriel unique.";
-			$liste_mots["legend"] = "Connexion";
+			$liste_mots["legend"] = "Saisir vos informations";
 			$liste_mots["usager"] = "Nom d'utilisateur :";
 			$liste_mots["pwd"] = "Mot de passe :";
 			$liste_mots["btn_login"] = "Se Connecter";
 			$liste_mots["btn_sign_up"] = "S'inscrire";
-			$liste_mots["btn_reset_pwd"] = "Mot de passe oublié ?";
 			$liste_mots["btn_reset"] = "Effacer";
+			$liste_mots["btn_reset_pwd"] = "Mot de passe oublié ?";
 			$liste_mots["btn_return"] = "Accueil";
 			
 		} elseif ($type_langue === 'english') {
@@ -35,8 +34,7 @@
 			$liste_mots["p1"] = "Welcome to the login page to see the statistic of poker between friends !";
 			$liste_mots["li1"] = "You must authenticate, to display the desired statistics";
 			$liste_mots["li2"] = "If you do not have an account, please create one.";
-			$liste_mots["li3"] = "Please specify a username and a unique email.";
-			$liste_mots["legend"] = "Logging";
+			$liste_mots["legend"] = "Enter your information";
 			$liste_mots["usager"] = "Username :";
 			$liste_mots["pwd"] = "Password :";
 			$liste_mots["btn_login"] = "Login";
@@ -85,7 +83,7 @@
 			case 8 : $message = "Au moment de vous connectez, vous n'avez rien saisie dans le champ «nom d'utilisateur» !";	break;
 			case 9 : $message = "Au moment de vous connectez, le nom d'utilisateur saisie n'existe pas !"; break;
 			case 10 : $message = "Au moment de vous connecter, votre mot de passe saisie est invalide avec votre utilisateur !<br>
-        		Si vous avez oublié votre mot de passe, veuillez appuyer sur le bouton «Mot de passe oublié ?» et suivre les instructions."; break;
+        		Si vous avez oublié votre mot de passe, veuillez appuyer sur le bouton « Mot de passe oublié ? » et suivre les instructions."; break;
 			case 13 : $message = "Attention les tous les champs sont vides !"; break;
 			case 15 : $message = "Attention les champs peuvent contenir seulement des caractères alphanumériques !"; break;
 			case 16 : $message = "Félicitation ! Votre compte a été crée avec succès !"; break;

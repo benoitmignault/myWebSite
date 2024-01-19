32da6047551d811d6e9a83f071af10d346b3f61f
Benoit Mignault <b.mignault@gmail.com>
2024-01-18 20:14:07 -0500

    Ajustement de la fct traduction

--- traduction/traduction-gestion-stats.php
+++ traduction/traduction-gestion-stats.php
@@ -1,63 +1,66 @@
 <?php
 	
 	/**
-	 * Fonction qui sera utiliser pour traduire le texte dans la page
+	 * Fonction qui sera utiliser pour traduire le texte dans la page et ainsi que les messages d'erreurs
 	 *
 	 * @param string $type_langue
+	 * @param int $situation
 	 * @return array
 	 */
-	function traduction(string $type_langue): array {
+	function traduction(string $type_langue, int $situation): array {
 		
+		$liste_mots = array(["lang" => "", 'btn_new' => "", 'title' => "", 'killer' => "", 'citron' => "",
+		                     'newJoueur' => "", 'gain' => "", 'h1' => "", 'victoire' => "", 'fini2e' => "", 'h3' => "",
+		                     'autre' => "", 'noId' => "", 'option' => "", 'joueur' => "", 'resultat' => "",
+		                     'btn_add' => "", 'btn_erase' => "", 'btn_loginPoker' => "", 'btn_login' => "",
+		                     'btn_return' => "");
+			
 		if ($type_langue === 'francais') {
-			$btn_new = "Ajouter le nouveau joueur";
-			$lang = "fr";
-			$newJoueur = "Nouveau joueur";
-			$title = "Page de gestion du poker et login";
-			$h1 = "Bienvenue à la page de gestion des statistiques du poker.";
-			$h3 = "Formulaire pour ajouter les statistiques d'un joueur.";
-			$option = "À sélectionner";
-			$joueur = "Joueur : ";
-			$resultat = "Résultat du classement : ";
-			$gain = "Gain Net :";
-			$victoire = "Victoire";
-			$fini2e = "Fini 2e";
-			$autre = "Autre";
-			$killer = "Prix killer :";
-			$citron = "Prix citron :";
-			$noId = "Numéro du tournois :";
-			$btn_add = "Ajouter";
-			$btn_erase = "Effacer";
-			$btn_loginPoker = "Voir les statistiques";
-			$btn_login = "Connexion à nouveau";
-			$btn_return = "Accueil";
+			$liste_mots["btn_new"] = "Ajouter le nouveau joueur";
+			$liste_mots["lang"] = "fr";
+			$liste_mots["newJoueur"] = "Nouveau joueur";
+			$liste_mots["title"] = "Page de gestion du poker et login";
+			$liste_mots["h1"] = "Bienvenue à la page de gestion des statistiques du poker.";
+			$liste_mots["h3"] = "Formulaire pour ajouter les statistiques d'un joueur.";
+			$liste_mots["option"] = "À sélectionner";
+			$liste_mots["joueur"] = "Joueur : ";
+			$liste_mots["resultat"] = "Résultat du classement : ";
+			$liste_mots["gain"] = "Gain Net :";
+			$liste_mots["victoire"] = "Victoire";
+			$liste_mots["fini2e"] = "Fini 2e";
+			$liste_mots["autre"] = "Autre";
+			$liste_mots["killer"] = "Prix killer :";
+			$liste_mots["citron"] = "Prix citron :";
+			$liste_mots["noId"] = "Numéro du tournois :";
+			$liste_mots["btn_add"] = "Ajouter";
+			$liste_mots["btn_erase"] = "Effacer";
+			$liste_mots["btn_loginPoker"] = "Voir les statistiques";
+			$liste_mots["btn_login"] = "Connexion à nouveau";
+			$liste_mots["btn_return"] = "Accueil";
 			
 		} elseif ($type_langue === 'english') {
-			$btn_new = "Add the new player";
-			$lang = "en";
-			$newJoueur = "New player :";
-			$title = "Poker management page and login";
-			$h1 = "Welcome to the User Management and Poker Statistics page.";
-			$h3 = "Form to add the statistics of a player.";
-			$option = "Select";
-			$resultat = "Ranking result : ";
-			$gain = "Profit";
-			$victoire = "Victory";
-			$killer = "Killer price";
-			$citron = "Lemons price";
-			$fini2e = "Runner-Up";
-			$autre = "Other";
-			$joueur = "Player : ";
-			$noId = "Tournament Id";
-			$btn_add = "Add";
-			$btn_erase = "Erase";
-			$btn_loginPoker = "View statistics";
-			$btn_login = "Back to login page";
-			$btn_return = "Back to Home";
+			$liste_mots["btn_new"] = "Add the new player";
+			$liste_mots["lang"] = "en";
+			$liste_mots["newJoueur"] = "New player :";
+			$liste_mots["title"] = "Poker management page and login";
+			$liste_mots["h1"] = "Welcome to the User Management and Poker Statistics page.";
+			$liste_mots["h3"] = "Form to add the statistics of a player.";
+			$liste_mots["option"] = "Select";
+			$liste_mots["resultat"] = "Ranking result : ";
+			$liste_mots["gain"] = "Profit";
+			$liste_mots["victoire"] = "Victory";
+			$liste_mots["killer"] = "Killer price";
+			$liste_mots["citron"] = "Lemons price";
+			$liste_mots["fini2e"] = "Runner-Up";
+			$liste_mots["autre"] = "Other";
+			$liste_mots["joueur"] = "Player : ";
+			$liste_mots["noId"] = "Tournament Id";
+			$liste_mots["btn_add"] = "Add";
+			$liste_mots["btn_erase"] = "Erase";
+			$liste_mots["btn_loginPoker"] = "View statistics";
+			$liste_mots["btn_login"] = "Back to login page";
+			$liste_mots["btn_return"] = "Back to Home";
 		}
 		
-		return ["lang" => $lang, 'btn_new' => $btn_new, 'title' => $title, 'killer' => $killer, 'citron' => $citron,
-		        'newJoueur' => $newJoueur, 'gain' => $gain, 'h1' => $h1, 'victoire' => $victoire, 'fini2e' => $fini2e, 'h3' => $h3,
-		        'autre' => $autre, 'noId' => $noId, 'option' => $option, 'joueur' => $joueur, 'resultat' => $resultat,
-		        'btn_add' => $btn_add, 'btn_erase' => $btn_erase, 'btn_loginPoker' => $btn_loginPoker, 'btn_login' => $btn_login,
-		        'btn_return' => $btn_return];
+		return $liste_mots;
 	}

a3ac34956f08c7970921961b3c3a0c984fd4a36b
Benoit Mignault <b.mignault@gmail.com>
2024-01-20 00:09:13 -0500

    Ajout d'un fichier commun divers avec fct divers

---
+++ includes/fct-divers.php
@@ -0,0 +1,25 @@
+<?php
+	
+	/**
+	 * Fonction qui sera utilisée partout, où il y a des validations
+	 * Retournera la valeur boolean true|false
+	 *
+	 * @param array $array_Champs
+	 * @return bool
+	 */
+	function verification_valeur_controle(array $array_Champs): bool{
+		
+		$erreur_presente = false;
+		// Validation que nous avons au moins une erreur, dans le lot
+		foreach ($array_Champs as $element) {
+			
+			if ($element === true) {
+				
+				$erreur_presente = true;
+				break; // Arrêter la boucle si un élément correspondant est trouvé
+			}
+		}
+		
+		return $erreur_presente;
+	}
+	

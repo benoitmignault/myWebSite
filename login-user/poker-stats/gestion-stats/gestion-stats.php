48305eecb71555159a932fda99b21f98a0c3f002
Benoit Mignault <b.mignault@gmail.com>
2024-01-20 11:36:32 -0500

    nettoyage espace

--- login-user/poker-stats/gestion-stats/gestion-stats.php
+++ login-user/poker-stats/gestion-stats/gestion-stats.php
@@ -810,7 +810,6 @@
 	        // On va faire la traduction, à la fin des GET & POST
 	        // La variable de situation est encore à 0 pour le GET, donc aucun message
 	        $array_Champs["liste_mots"] = traduction($array_Champs["type_langue"], $array_Champs["situation"]);
-         
         }
     }
     

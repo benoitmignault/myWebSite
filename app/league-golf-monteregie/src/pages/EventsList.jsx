b1fe2a428c301a9c43194c8cf9773da4ce6da9d0
Benoit Mignault <b.mignault@gmail.com>
2026-05-16 20:45:33 -0400

    feat: Introduce basic EventsList page component

---
+++ app/league-golf-monteregie/src/pages/EventsList.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+function EventsList() {
+
+    return (
+        <div>
+            <h2>
+                Événements
+            </h2>
+        </div>
+    );
+}
+
+export default EventsList;

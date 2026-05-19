f7d7f0aa6ebeb247d385b0d83537e283b7a84c8c
Benoit Mignault <b.mignault@gmail.com>
2026-05-19 09:30:07 -0400

    feat: Implement page load logging

--- app/league-golf-monteregie/src/App.jsx
+++ app/league-golf-monteregie/src/App.jsx
@@ -1,32 +1,49 @@
+import { useEffect } from "react";
 import Standings from "./pages/Standings";
 import EventsList from "./pages/EventsList";
 
 function App() {
-  return (
-    <div>
-      <h1>Ligue de Golf Montérégie</h1>
-      <div className="main-container">        
-        <div className="sub-container">
-          <Standings />
-        </div>
-        <div className="sub-container">
-          <EventsList />
-        </div>
-      </div>
-     
-      <button
-        className="scroll-top"
-        onClick={() =>
-            window.scrollTo({
-                top: 0,
-                behavior: "smooth"
-            })
-        }
-      > 
-        ↑
-      </button>      
-    </div>
-  );
+
+	// Utiliser useEffect pour envoyer une requête à l'API de logging à chaque fois que la page est chargée
+	useEffect(() => {
+		fetch("https://localhost/api/log-action.php",
+			{ 
+				method: "POST",
+				headers: {"Content-Type": "application/json"},
+				body: JSON.stringify({
+					action_type: "page_load",
+					target_id: null,
+					target_name: "Affichage page principale"
+				})
+			}
+		);
+	}, []);
+
+	return (
+		<div>
+			<h1>Ligue de Golf Montérégie</h1>
+			<div className="main-container">        
+				<div className="sub-container">
+					<Standings />
+				</div>
+				<div className="sub-container">
+					<EventsList />
+				</div>
+			</div>
+			
+			<button
+				className="scroll-top"
+				onClick={() =>
+					window.scrollTo({
+						top: 0,
+						behavior: "smooth"
+					})
+				}
+			> 
+				↑
+			</button>      
+		</div>
+	);
 }
 
 export default App;

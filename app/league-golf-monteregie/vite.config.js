b0fbdac02a0484306aa4f26e5f906eb1ab1f7819
Benoit Mignault <b.mignault@gmail.com>
2026-05-19 22:12:20 -0400

    config(vite): Adjust base path for Monteregie league app deployment

--- app/league-golf-monteregie/vite.config.js
+++ app/league-golf-monteregie/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app/league-golf-monteregie/'
+  // Définir la base pour que les ressources soient correctement chargées 
+  // même si l'application est servie à partir d'un sous-répertoire
+  // Surtout que en PROD : https://golf.benoitmignault.ca/league-monteregie
+  base: '/league-golf-monteregie/'
 })

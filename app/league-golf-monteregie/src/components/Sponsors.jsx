ae714ebb8dcda7bf931f891825eed28d8b0d0687
Benoit Mignault <b.mignault@gmail.com>
2026-05-20 09:29:59 -0400

    feat: Add `logo` property to sponsor objects

--- app/league-golf-monteregie/src/components/Sponsors.jsx
+++ app/league-golf-monteregie/src/components/Sponsors.jsx
@@ -3,36 +3,42 @@ function Sponsors() {
     const sponsors = [
         {
             name: "Apex Golf",
+            logo: null,
             website: "https://apex-golf.ca/",
             facebook: "https://www.facebook.com/TeeTime.ca",
             instagram: "https://www.instagram.com/tee.time.ca/"
         },
         {
             name: "Station GO",
+            logo: null,
             website: "https://stationgo.ca/",
             facebook: "https://www.facebook.com/stationgo.ca",
             instagram: "https://www.instagram.com/stationgo.ca/"
         },
         {
             name: "Groupe Golf Montérégie",
+            logo: null,
             website: null,
             facebook: "https://www.facebook.com/groups/1029936997443683",
             instagram: null
         },
         {
             name: "Toucani",
+            logo: null,
             website: null,
             facebook: null,
             instagram: null
         },
         {
             name: "Mr Tee",
+            logo: null,
             website: null,
             facebook: null,
             instagram: null
         },
         {
             name: "FlexiGolf",
+            logo: null,
             website: "https://flexigolf.ca/",
             facebook: "https://www.facebook.com/FlexiGolfQc",
             instagram: "https://www.instagram.com/flexigolf/"

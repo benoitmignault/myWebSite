4f910556a3cac1ac3c1f4ecafc1cdbe7f6b9553e
Benoit Mignault <b.mignault@gmail.com>
2026-05-20 20:23:42 -0400

    feat(contact): Add ContactSection component

---
+++ app/league-golf-monteregie/src/components/ContactSection.jsx
@@ -0,0 +1,9 @@
+function ContactSection() {
+    return (
+        <div className="contact-section">
+            <h2>Contactez-nous</h2>
+        </div>
+    );
+}
+
+export default ContactSection;

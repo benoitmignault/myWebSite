5df99b56b4aa37d4d6869dffe2e365c8612a8053
Benoit Mignault <b.mignault@gmail.com>
2026-05-22 11:29:06 -0400

    renommage /league-monteregie et non golf

--- app/league-golf-monteregie/vite.config.js
+++ app/league-golf-monteregie/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   // Définir la base pour que les ressources soient correctement chargées 
   // même si l'application est servie à partir d'un sous-répertoire
   // Surtout que en PROD : https://golf.benoitmignault.ca/league-monteregie
-  base: '/league-golf-monteregie/'
+  base: '/league-monteregie/'
 })

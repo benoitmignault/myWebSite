fe364a377d36c41f7c2ad51d1b85cdfc0a868f79
Benoit Mignault <b.mignault@gmail.com>
2026-05-26 00:37:35 -0400

    Ajout de la 3 semaine pour histyorique de previous et currents

--- golf.benoitmignault.ca/league-monteregie/src/table_sql/player_event_history.sql
+++ golf.benoitmignault.ca/league-monteregie/src/table_sql/player_event_history.sql
@@ -122,3 +122,46 @@ VALUES
 (2, 14, NULL, 16, 0, 245, 245, NULL, 22.0),
 -- Mathieu Robidas
 (2, 12, NULL, 18, 0, 210, 210, NULL, 7.0);
+
+-- Les données après la semaine 3 de la saison 2026 de la league de golf de la Montérégie
+INSERT INTO player_event_history (
+
+    event_id,
+    player_id,
+    previous_position,
+    current_position,
+    previous_fedex_points,
+    current_fedex_points,
+    fedex_points_gained,
+    previous_handicap,
+    current_handicap
+)
+VALUES
+-- Martin Taillon
+(3, 16, 15, 11, 245, 545, 300, 20.0, 13.0),
+-- Jean-Pierre Duval
+(3, 18, 5, 3, 464, 754, 290, 18.0, 16.0),
+-- Benoît Mignault
+(3, 8, 3, 2, 480, 760, 280, 11.9, 11.0),
+-- Sylvain Gervais
+(3, 15, 14, 12, 245, 515, 270, 14.0, 14.0),
+-- Robert Gaboriault
+(3, 9, 7, 6, 421, 681, 260, 17.0, 17.0),
+-- Daniel Lefebvre
+(3, 4, 6, 5, 463, 713, 250, 15.0, 15.0),
+-- Nicolas Carrière
+(3, 10, 2, 4, 500, 735, 235, 7.0, 7.0),
+-- Jean-Sébastien Patenaude
+(3, 17, 13, 14, 245, 480, 235, 12.0, 12.0),
+-- Luc Grimard
+(3, 6, 9, 9, 374, 589, 215, 28.0, 28.0),
+-- Mai-Révée Dolceb
+(3, 3, 8, 8, 404, 619, 215, 33.9, 33.0),
+-- Maxime Paulin
+(3, 13, 11, 13, 300, 502, 202, 15.0, 15.0),
+-- Robert Labelle
+(3, 7, 1, 1, 585, 775, 190, 10.0, 10.0),
+-- Monia Roulier
+(3, 2, 4, 7, 479, 669, 190, 28.0, 28.0),
+-- Eric Grimard
+(3, 5, 10, 10, 368, 546, 178, 17.0, 17.0);

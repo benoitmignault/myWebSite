dec0247117b7a87849ee7394be98c27c51832751
Benoit Mignault <b.mignault@gmail.com>
2026-05-26 23:08:49 -0400

    setter les variables de composants

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/Login.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/Login.jsx
@@ -1,4 +1,38 @@
+import { useEffect, useState } from "react";
+import React from "react";
+import { API_BASE_URL } from "../config";
+
+
+
+/**
+ * Composant de connexion pour les administrateurs et les sous-administrateurs
+ * 
+ * @description 
+ * Affiche un formulaire de connexion pour les administrateurs et les sous-administrateurs. 
+ * Permet aux utilisateurs de saisir leurs informations d'identification et de se connecter à l'interface d'administration.
+ * 
+ * @returns 
+ */
 function Login() {
+
+    // États pour stocker le username du formulaire de connexion
+    const [username, setUsername] = useState("");
+
+    // États pour stocker le password du formulaire de connexion
+    const [password, setPassword] = useState("");
+
+    // État pour indiquer si la connexion est en cours de traitement
+    const [loading, setLoading] = useState(false);
+
+    // État pour stocker les messages d'erreur de connexion
+    const [error, setError] = useState("");
+
+
+
+
+
+
+
 
     return (
         <div>

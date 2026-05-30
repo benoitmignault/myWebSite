deb23635c0dcd2d4a5198a7e3453ce455eed12b0
Benoit Mignault <b.mignault@gmail.com>
2026-05-30 00:02:46 -0400

    feat(admin): Setup basic structure for Add Event form

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/EventsSection.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/EventsSection.jsx
@@ -1,20 +1,42 @@
-// Sous composant de la page d'administration qui permet d'ajouter de nouveaux événements
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../../config";
 import "./admin.css";
 
+
+
+/**
+ * Composant de gestion des événements pour les administrateurs
+ * 
+ * @description 
+ * Affiche un formulaire pour ajouter un événement à la ligue
+ * 
+ * @returns 
+ */
 function EventsSection() {
+
+    // Utilisation de useNavigate pour rediriger l'utilisateur vers le bon lien en cas de session invalide
+    const navigate = useNavigate();
+
+    // État pour le nom de l'événement à ajouter
+    const [eventName, setEventName] = useState("");
+
+    // État pour le lieu de l'événement à ajouter
+    const [eventLocation, setEventLocation] = useState("");
+
+    // État pour la date de l'événement à ajouter
+    const [eventDate, setEventDate] = useState("");
+
+    // État pour l'URL du site web de l'événement à ajouter
+    const [eventUrl, setEventUrl] = useState("");
+
+
+
 
     return (
         <div className="admin-section-card">
-
-            <h2>Gestion des Événements</h2>
-
-            <p className="admin-section-description">
-                Ajouter et gérer les événements de la saison.
-            </p>
-
-            <button className="admin-button">
-                Ajouter un événement
-            </button>
+            <h2>Section pour ajouter un evenement</h2>
+            
 
         </div>
     );

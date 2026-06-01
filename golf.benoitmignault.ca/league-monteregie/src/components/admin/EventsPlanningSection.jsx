4d2660354b6e83daeff39250e9949210f0adbabc
Benoit Mignault <b.mignault@gmail.com>
2026-06-01 09:32:03 -0400

    feat(admin): Implement next event data fetching logic

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/EventsPlanningSection.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/EventsPlanningSection.jsx
@@ -20,15 +20,72 @@ function EventsPlanningSection() {
     // Utilisation de useNavigate pour rediriger l'utilisateur vers le bon lien en cas de session invalide
     const navigate = useNavigate();
 
-    // 
+    // ÉTat pour stocker la réponse de l'API pour le prochain évenement qui sera en cours, avec les détails de cet évenement
+    const [nextEvent, setNextEvent] = useState(null);
 
 
 
+
+    // État pour stocker les messages d'erreur en prévision de l'ajout d'un joueur à un événement
+    const [error, setError] = useState("");
+
+    // État pour stocker un message de succès lors de l'ajout d'un événement à la ligue
+    const [successMessage, setSuccessMessage] = useState("");
 
 
     // État pour empêcher de faire l'ajout plusieurs fois de suite d'un joueur à un évenement 
     // si on clique sur le bouton, en attendant la réponse de l'API
     const [loading, setLoading] = useState(false);
+
+
+
+    // Fonction pour charger les détails du prochain évenement qui sera en cours, avec une requête à l'API get-next-event.php
+    const loadEvent = async () => {
+
+        // Juste au cas ou, on va setter le loading à true pour éviter que l'utilisateur puisse cliquer plusieurs fois sur le bouton d'ajout d'un joueur à un évenement
+        setLoading(true);
+
+        try {
+            const response = await fetch(`${API_BASE_URL}/admin/get-next-event.php`,
+                {
+                    credentials: "include"
+                }
+            );
+
+            // Si la réponse de l'API indique que la session est invalide, 
+            // rediriger le gestionnaire vers la page de connexion
+            if (response.status === 401) {
+
+                setError("Votre session a expiré, vous allez être redirigé vers la page de connexion.");
+                setTimeout(() => {navigate("/league-monteregie/admin");}, 3000);
+                return;
+            }
+
+            // Sinon, on a un résultat valide du retour de l'API
+            const data = await response.json();
+
+            if (data.success) {
+
+                // Stocker les détails du prochain évenement qui sera en cours dans l'état nextEvent
+                setNextEvent(data.event);
+
+            } else {
+
+                // Erreur lors du chargement de l'évenement
+                setError(data.message);
+            }
+
+        } catch (err) {
+
+            console.error(err);
+            setError("Une erreur est survenue lors du chargement de l'événement.");
+
+        } finally {
+
+            setLoading(false);
+        }        
+    };
+
 
 
     

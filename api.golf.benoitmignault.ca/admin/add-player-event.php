79bf7784ae029548a5df9beb204c6840e4246558
Benoit Mignault <b.mignault@gmail.com>
2026-06-01 23:51:18 -0400

    feat(admin): Add API endpoint to add player to event with validations and history prep

---
+++ api.golf.benoitmignault.ca/admin/add-player-event.php
@@ -0,0 +1,147 @@
+<?php
+
+// Ce fichier sera utilisé pour ajouter un joueur à un événement spécifique. Il reçoit les données du frontend,
+// les valide, puis les insère dans la table event_players.
+// Ensuite, on récupère la position générale actuelle, le nombre de points Fedex et 
+// le handicap en prévision du tournoi à venir.
+
+// Inclut les informations nécessaires pour CORS
+include(__DIR__ . "/../includes/cors.php");
+
+// Inclut les informations pour vérifier la session d'administrateur
+include(__DIR__ . "/auth/check-admin-session.php");
+
+// Inclut la fonction de connexion à la base de données
+include(__DIR__ . "/../includes/fct-connexion-bd.php");
+
+// S'assurer que la requête est une requête POST
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+
+    http_response_code(405);
+    echo json_encode(["success" => false, "message" => "Méthode non autorisée."]);
+    exit();
+}
+
+// Si on passe la validation du POST et que la session est valide, on poursuit avec la lecture des données reçues, 
+// la validation des données, la connexion à la base de données, 
+// l'insertion du joueur à l'événement dans la base de données et le retour d'une réponse JSON indiquant.
+$data = json_decode(file_get_contents("php://input"), true);
+
+// Vérifier si les données ont été reçues
+if (!$data) {
+
+    http_response_code(400);
+    echo json_encode(["success" => false, "message" => "Aucune donnée reçue"]);    
+    exit();
+}
+
+// Récupérer les données envoyées depuis le frontend
+$eventId = $data['event_id'];
+$playerId = $data['player_id'];
+$teamId = $data['team_id'];
+
+// Validation des données
+if ($eventId <= 0 || $playerId <= 0 || $teamId <= 0) {
+
+    http_response_code(400);
+
+    echo json_encode(["success" => false, "message" => "Informations invalides."]);
+    exit();
+}
+
+// Établir une connexion à la base de données de la ligue de golf en montérégie
+$conn = connexion_league_golf_monteregie();
+
+if (!$conn) {
+
+    http_response_code(500);
+    echo json_encode(["success" => false, "message" => "Erreur de connexion à la base de données."]);
+    exit();
+}
+
+// Vérification que event existe avant aller plus loin
+$select = "SELECT id, is_closed ";
+$from = "FROM events ";
+$where = "WHERE id = ?";
+$sql = $select . $from . $where;
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $eventId);
+$stmt->execute();
+$result = $stmt->get_result();
+
+if ($result->num_rows === 0) {
+
+    http_response_code(404);
+    echo json_encode(["success" => false, "message" => "Événement introuvable."]);
+
+    $stmt->close();
+    $conn->close();
+    exit();
+}
+
+// Vérification que l'événement n'est pas déjà fermé avant aller plus loin
+$event = $result->fetch_assoc();
+
+if ($event["is_closed"] == 1) {
+
+    http_response_code(400);
+    echo json_encode(["success" => false, "message" => "Cet événement est déjà fermé."]);
+
+    $stmt->close();
+    $conn->close();
+    exit();
+}
+
+// On va récupérer current_position, current_fedex_points, current_handicap pour aller les insérer 
+// dans la table player_event_history sous la notion de previous_position, previous_fedex_points,
+//  previous_handicap pour garder un historique de l'évolution du joueur au fil des événements
+$select = "SELECT current_position, current_fedex_points, current_handicap ";
+$from = "FROM player_event_history ";
+$where = "WHERE player_id = ?";
+$orderBy = "ORDER BY event_id DESC ";
+$limit = "LIMIT 1";
+$sql = $select . $from . $where . $orderBy . $limit;
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $playerId);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$history = $result->fetch_assoc();
+
+// Cas particulier pour le premier événement d'un joueur, où il n'a pas encore de position, 
+// de points Fedex ou de handicap enregistré dans la table player_event_history.
+if ($history) {
+
+    $previousPosition = $history["current_position"];
+    $previousFedexPoints = $history["current_fedex_points"];
+    $previousHandicap = $history["current_handicap"];
+
+} else {
+
+    $previousPosition = 0;
+    $previousFedexPoints = 0;
+
+    // Il faudra aller chercher le handicap actuel du joueur dans la table players pour l'insérer
+    // dans player_event_history et la table event_players, car il n'a pas encore de handicap enregistré 
+    // dans player_event_history pour un évent précédent.
+    $select = "SELECT handicap_league ";
+    $from = "FROM players ";
+    $where = "WHERE id = ?";
+    $sql = $select . $from . $where;
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $playerId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $player = $result->fetch_assoc();
+    $previousHandicap = $player["handicap_league"];
+}
+
+
+
+
+
+
+
+}

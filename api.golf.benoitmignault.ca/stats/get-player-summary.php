b121d9e67e5f33094315184fae13e698f429ddfe
Benoit Mignault <b.mignault@gmail.com>
2026-06-19 16:37:18 -0400

    Correction de la deuxième requete pour celle «préparée»

--- api.golf.benoitmignault.ca/stats/get-player-summary.php
+++ api.golf.benoitmignault.ca/stats/get-player-summary.php
@@ -88,3 +88,104 @@ $row = $result->fetch_assoc();
 $playerInfo["full_name"] = $row["firstname"] . " " . $row["lastname"];
 $playerInfo["handicap"] = $row["handicap_league"];
 $playerInfo["average_score"] = $row["average_score"];
+
+// Libérer les ressources associées au résultat de la première requête
+mysqli_free_result($result);
+$stmt->close();
+
+// Deuxième requete SQL viendra récupérer les trophées qu'il a remportés.
+$select = "SELECT
+    SUM(CASE WHEN position = 1 THEN 1 ELSE 0 END) AS gold_trophies,
+    SUM(CASE WHEN position = 2 THEN 1 ELSE 0 END) AS silver_trophies,
+    SUM(CASE WHEN position = 3 THEN 1 ELSE 0 END) AS bronze_trophies ";
+
+$from = "FROM round_results ";
+$where = "WHERE player_id = ?";
+$sql = $select . $from . $where;
+
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $playerId);
+
+// Vérifier si la requête a réussi
+if (!$stmt->execute()) {
+
+    error_log($stmt->error);
+    
+    http_response_code(500);
+    echo json_encode(["success" => false, "message" => "Erreur lors de l'exécution de la requête."]); 
+    
+    // Fermer la connexion au résultat du insert dans la base de données et la connexion à la base de données
+    $stmt->close();
+    $conn->close();
+    exit();
+}
+
+$result = $stmt->get_result();
+
+// Il ne sert à rien de vérifier si des résultats ont été trouvés, 
+// car même si le joueur n'a pas de trophées ou de points FedEx, 
+// la requête retournera quand même une ligne avec des valeurs à 0, mais surtout que rendu ici, on sait que le joueur existe, 
+// car on a vérifié ça dans la première requête SQL.
+
+// Récupérer les informations du joueur
+$row = $result->fetch_assoc();
+
+// Remplir les informations du joueur dans la structure de données
+$playerInfo["gold_trophies"] = $row["gold_trophies"];
+$playerInfo["silver_trophies"] = $row["silver_trophies"];
+$playerInfo["bronze_trophies"] = $row["bronze_trophies"];
+
+// Libérer les ressources associées au résultat de la deuxième requête
+mysqli_free_result($result);
+$stmt->close();
+
+// On va reprendre la requête SQL utilisé dans standing.php 
+// pour récupérer le classement de la coupe Fedex du joueur, 
+// mais cette fois-ci, on va filtrer les résultats pour n'avoir que le joueur dont on veut le résumé.
+$select = "SELECT p.id, COALESCE(SUM(r.fedex_points), 0) AS total_points ";
+// Utilisation de LEFT JOIN pour inclure tous les joueurs, même ceux sans résultats de tournament
+$from = "FROM players p LEFT JOIN round_results r ON p.id = r.player_id ";
+$groupBy = "GROUP BY p.id ";
+$orderBy = "ORDER BY total_points DESC, p.handicap_league ASC, p.firstname ASC";
+$sql = $select . $from . $groupBy . $orderBy;
+
+// Exécuter la requête SQL
+$result = $conn->query($sql);
+
+// Vérifier si la requête a réussi
+if (!$result) {
+
+    http_response_code(500);
+    echo json_encode(["success" => false, "message" => "Erreur lors de l'exécution de la requête."]);
+    $conn->close();
+    exit();
+}
+
+$position = 1;
+
+// On va essayer de trouve rnotre joueur dans les résultats de la requête 
+// pour récupérer sa position dans le classement de la coupe FedEx par un incrémentation 
+// d'une variable $position à chaque itération de la boucle, 
+// et une fois qu'on trouve notre joueur, on remplit sa position et 
+// ses points FedEx dans la structure de données $playerInfo, et on sort de la boucle.
+while ($row = $result->fetch_assoc()) {
+
+    // Si on trouve notre joueur dans les résultats de la requête
+    if ($row["id"] == $playerId) {
+
+        $playerInfo["fedex_position"] = $position;
+        $playerInfo["fedex_points"] = $row["total_points"];
+        break;
+    }
+
+    // Incrémenter la position pour le prochain joueur
+    $position++;
+}
+
+// Fermer la connexion à la base de données
+$conn->close();
+
+http_response_code(200);
+
+// Retourner les données au format JSON
+echo json_encode(["success" => true, "playerInfo" => $playerInfo]);

a453c974410dbc9f2064690d8f8a31b51a9cf65c
Benoit Mignault <b.mignault@gmail.com>
2026-06-18 22:39:27 -0400

    Création du skelette pour playersummary

---
+++ golf.benoitmignault.ca/league-monteregie/src/components/stats/PlayerSummary.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { API_BASE_URL } from "../../config";
+
+/**
+ * 
+ * 
+ * @param {integer} selectedPlayerId 
+ * @returns 
+ */
+
+function PlayerSelector({ selectedPlayerId }) {
+
+
+    return (
+        <div>
+            
+        </div>
+
+
+
+
+
+
+
+
+    );
+}
+
+export default PlayerSelector;

242de3e594d9de46104e75ae215a9636b1902d38
Benoit Mignault <b.mignault@gmail.com>
2026-06-19 15:26:25 -0400

    Ajout commentaire et du useeffet en prévision de l'affichage

--- golf.benoitmignault.ca/league-monteregie/src/components/stats/PlayerSummary.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/stats/PlayerSummary.jsx
@@ -6,14 +6,48 @@ import { API_BASE_URL } from "../../config";
  * comme son nom, son handicap, sa moyenne brute, le nombre de trophées gagnés, 
  * le nombre de tournois auxquels il a participé, etc.
  * 
+ * En trois section :
+ * Informations du joueur
+     Nom complet
+     Handicap actuel
+     Moyenne actuelle
+
+   Position dans la ligue
+     Position FedEx actuelle
+     Points FedEx actuels
+   Récompenses
+     🏆 Or
+     🥈 Argent
+     🥉 Bronze
+ * 
  * @param {integer} playerId 
  * @returns 
  */
 
 function PlayerSummary({ playerId }) {
 
+    const [playerSummary, setPlayerSummary] = useState(null);
 
-    console.log(playerId);
+
+    const [error, setError] = useState("");
+
+
+    
+    // Utilisé dès le chargement du composant pour récupérer les données du joueur sélectionné,
+    // et les afficher dans la section d'informations du joueur
+    useEffect(() => {
+
+        // Il faut utiliser une notion asynchrone pour charger les données, en raison de l'utilisation 
+        const initializeData = async () => {
+
+            // Charger les données du joueur sélectionné pour les afficher dans la section d'informations du joueur
+            //await loadAllPlayers();                            
+        };
+        
+        // 
+        initializeData();
+
+    }, []);
 
     return (
         <div>

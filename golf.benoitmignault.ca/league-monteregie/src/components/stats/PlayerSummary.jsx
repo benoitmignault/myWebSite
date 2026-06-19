44289f7f2db3be59853a8ab9bf5032c46218dcdb
Benoit Mignault <b.mignault@gmail.com>
2026-06-19 14:50:40 -0400

    Explication du composant

--- golf.benoitmignault.ca/league-monteregie/src/components/stats/PlayerSummary.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/stats/PlayerSummary.jsx
@@ -2,13 +2,15 @@ import { useState, useEffect } from "react";
 import { API_BASE_URL } from "../../config";
 
 /**
- * 
+ * Composant pour afficher un résumé des statistiques d'un joueur sélectionné, 
+ * comme son nom, son handicap, sa moyenne brute, le nombre de trophées gagnés, 
+ * le nombre de tournois auxquels il a participé, etc.
  * 
  * @param {integer} playerId 
  * @returns 
  */
 
-function PlayerSelector({ playerId }) {
+function PlayerSummary({ playerId }) {
 
 
     console.log(playerId);

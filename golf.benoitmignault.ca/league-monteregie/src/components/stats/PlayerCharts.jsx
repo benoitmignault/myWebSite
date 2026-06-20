1fa10c770c5c7128879713e1c5ecca4e550011da
Benoit Mignault <b.mignault@gmail.com>
2026-06-19 21:58:12 -0400

    Réorganisation de la section graphique pour cacher si aucun resultat

--- golf.benoitmignault.ca/league-monteregie/src/components/stats/PlayerCharts.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/stats/PlayerCharts.jsx
@@ -11,12 +11,9 @@ function PlayerCharts({ selectedPlayerId }) {
 
     
 
-    const loadChartData = async () => {
+    const loadChartData = async (selectedPlayerId) => {
 
         try {
-
-            setLoading(true);
-
             // TODO : API
 
             setChartData([]);
@@ -26,49 +23,40 @@ function PlayerCharts({ selectedPlayerId }) {
 
             console.error(error);
 
-        }
-        finally {
-
-            setLoading(false);
-
-        }
+        }        
     };
 
     useEffect(() => {
 
-        loadChartData();
+        loadChartData(selectedPlayerId);
 
     }, [selectedPlayerId]);
 
-    if (loading) {
-
-        return (
-            <div className="chart-loading">
-                Chargement des graphiques...
-            </div>
-        );
-    }    
-
     return (
         <div className="player-charts-container">
-
             <h2>Évolution du joueur</h2>
+            {
+                chartData.length > 0 && (
+                    <>
+                        {/* TODO : Afficher les graphiques avec les données de chartData */}
+                        {/* Graphique 1 */}
+                        <div className="chart-card">
+                            Graphique évolution de l'handicap et de la moyenne de score
+                        </div>
 
-            {/* Graphique 1 */}
-            <div className="chart-card">
-                Graphique évolution de l'handicap et de la moyenne de score
-            </div>
+                        {/* Graphique 2 */}
+                        <div className="chart-card">
+                            Graphique évolution du classement FedEx
+                        </div>
 
-            {/* Graphique 2 */}
-            <div className="chart-card">
-                Graphique évolution du classement FedEx
-            </div>
-
-            {/* Graphique 3 */}
-            <div className="chart-card">
-                Graphique points FedEx cumulés
-            </div>
-
+                        {/* Graphique 3 */}
+                        <div className="chart-card">
+                            Graphique points FedEx cumulés
+                        </div>
+                    </>
+                    
+                )}
+            
         </div>
     );
 }

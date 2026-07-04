289fc74d4e411f90bcd4ea66268d38e126e0d664
Benoit Mignault <b.mignault@gmail.com>
2026-07-04 13:01:29 -0400

    mise en commentaire des analyses de visualisations des packages

--- golf.benoitmignault.ca/league-monteregie/vite.config.js
+++ golf.benoitmignault.ca/league-monteregie/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import { visualizer } from "rollup-plugin-visualizer";
+// import { visualizer } from "rollup-plugin-visualizer";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), visualizer({open: true, gzipSize: true})],
+  plugins: [react(),  /* visualizer({open: true, gzipSize: true})] */ ],
   
   // Définir la base pour que les ressources soient correctement chargées 
   // même si l'application est servie à partir d'un sous-répertoire

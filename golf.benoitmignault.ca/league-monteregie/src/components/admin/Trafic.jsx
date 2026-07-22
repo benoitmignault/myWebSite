07b2a2bbe4edb520ef322b4c8de143ade89f0316
Benoit Mignault <b.mignault@gmail.com>
2026-07-22 00:09:59 -0400

    ajout d'un squelette pour la nouvelle page analyse de trafic

---
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/Trafic.jsx
@@ -0,0 +1,39 @@
+// import { useNavigate } from "react-router-dom";
+
+import Footer from "../Footer";
+import '../../css/admin.css'
+
+const Trafic = () => {
+
+    // const navigate = useNavigate();
+
+    return (
+        <div>
+
+            <div className="dashboard-container">
+
+                <h1 className="gestion-title">
+                    Activité du site
+                </h1>
+
+                {/* 0 - Sélection de la période */}
+
+                {/* 1 - Site Web */}
+                {/* 1.1 Sommaire */}
+                {/* 1.2 Top 10 */}
+                {/* 1.3 Visiteurs uniques */}
+
+                {/* 2 - Partenaires */}
+                {/* 2.1 Sommaire */}
+                {/* 2.2 Répartition par média */}
+                {/* 2.3 Classement */}
+
+            </div>
+
+            <Footer />
+
+        </div>
+    );
+};
+
+export default Trafic;

84ebb7989af7e7818ea751cfeda3b15234ec34e9
Benoit Mignault <b.mignault@gmail.com>
2026-07-22 00:20:44 -0400

    ajustement des liens du CSS et footer en bas de page

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/Analytics.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/Analytics.jsx
@@ -1,7 +1,7 @@
 // import { useNavigate } from "react-router-dom";
 
-import Footer from "../Footer";
-import '../../css/admin.css'
+import Footer from "../../Footer";
+import '../../../css/admin.css'
 
 const Trafic = () => {
 

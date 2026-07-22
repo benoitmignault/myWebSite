f5c948d7192a82a02c7aae614c3d0d9e902a2ff6
Benoit Mignault <b.mignault@gmail.com>
2026-07-22 00:02:32 -0400

    Refactor: Organize player management API endpoint

--- api.golf.benoitmignault.ca/admin/management/players/add-player.php
+++ api.golf.benoitmignault.ca/admin/management/players/add-player.php
@@ -4,13 +4,13 @@
 // les valide, puis les insère dans la base de données.
 
 // Inclut les informations nécessaires pour CORS
-include(__DIR__ . "/../includes/cors.php");
+include(__DIR__ . "/../../../includes/cors.php");
 
 // Inclut les informations pour vérifier la session d'administrateur
-include(__DIR__ . "/auth/check-admin-session.php");
+include(__DIR__ . "/../../auth/check-admin-session.php");
 
 // Inclut la fonction de connexion à la base de données
-include(__DIR__ . "/../includes/fct-connexion-bd.php");
+include(__DIR__ . "/../../../includes/fct-connexion-bd.php");
 
 // S'assurer que la requête est une requête POST
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {

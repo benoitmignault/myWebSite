f770781c1fad8dc0b9335ff5a61a739290d71b64
Benoit Mignault <b.mignault@gmail.com>
2026-07-25 19:44:09 -0400

    ajout une icone  + correction de la JAVA doc

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/PeriodSelector.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/PeriodSelector.jsx
@@ -16,30 +16,20 @@ const PERIOD_OPTIONS = [
  * Ce composant affiche un menu déroulant permettant à l'utilisateur de choisir une période spécifique 
  * pour filtrer les données affichées dans le tableau des statistiques.
  * 
- * @param {value} selectedPeriod - La période actuellement sélectionnée
- * @param {function} setSelectedPeriod - Fonction pour mettre à jour la période sélectionnée
- * @returns
+ * @param {string} selectedPeriod - La période actuellement sélectionnée.
+ * @param {function} setSelectedPeriod - Fonction pour mettre à jour la période sélectionnée.
  */
-function PeriodSelector({ selectedPeriod, setSelectedPeriod }) {
-
-    // Définition des options de période disponibles pour le menu déroulant
-    const PERIOD_OPTIONS = [
-        { value: "all", label: "Depuis le début" },
-        { value: "today", label: "Aujourd'hui" },
-        { value: "7", label: "7 derniers jours" },
-        { value: "30", label: "30 derniers jours" },
-        { value: "90", label: "90 derniers jours" }
-    ];
+function PeriodSelector({ selectedPeriod, setSelectedPeriod}) {
 
     return (
         <div className="analytics-period">
-
+            <LuCalendarRange />
             <label htmlFor="period">Période</label>
 
             <select
                 id="period"
                 value={selectedPeriod}
-                onChange={(e) => setSelectedPeriod(e.target.value)}
+                onChange={(e) => setSelectedPeriod (e.target.value)}
             >
                 {PERIOD_OPTIONS.map(period => (
                     <option key={period.value} value={period.value}>{period.label}</option>

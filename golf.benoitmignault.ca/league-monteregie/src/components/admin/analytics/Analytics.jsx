872f5f6c16676f229cebbd35524afcff87cfc906
Benoit Mignault <b.mignault@gmail.com>
2026-07-25 16:47:15 -0400

    Ajout de la NavBar et de la verif si user est connect

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/Analytics.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/Analytics.jsx
@@ -1,15 +1,95 @@
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { FaHouse } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
+import { MdAdminPanelSettings } from "react-icons/md";
 
+import { API_BASE_URL } from "../../../config";
 import Footer from "../../Footer";
 import '../../../css/admin.css'
 
 function Trafic() {
 
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
+
+
+
+    // Fonction pour gérer la déconnexion de l'administrateur et avec une redirection en fonction du lien qu'on a cliqué
+    const handleLogout = async (redirectTo) => {
+
+        try {
+            await fetch(`${API_BASE_URL}/admin/auth/logout.php`,
+                {
+                    method: "POST",
+                    credentials: "include"
+                }
+            );
+
+        } finally {
+
+            // Rediriger l'utilisateur vers la page de connexion après la déconnexion, 
+            // ou vers la page d'accueil s'il a cliqué sur le lien de la maison
+            navigate(redirectTo);
+        }
+    };
+
+
+    // Avant de loader la page de statisitéques du site, on doit vérifier que l'administrateur est bien connecté
+    // en vérifiant la session avec l'API check-session.php.
+    useEffect(() => {
+        fetch(`${API_BASE_URL}/admin/auth/check-session.php`, {credentials: "include"})
+        /**
+         * response : status, HTTP, headers, ok, etc.
+         * data : le corps de la réponse, qui est un objet JSON contenant les données retournées par l'API,
+         * généralement avec une structure comme { success: boolean, message: string, ... }
+         */
+        .then((response) => {
+
+            if (!response.ok) {
+                throw new Error("Session invalide");
+            }
+
+            // Changement du titre de la page lorsque le composant du dashboard est monté
+            document.title = "Activité du site";
+
+            // Changement du favicon de la page pour le dashboard
+            const favicon = document.getElementById("dynamic-favicon");
+
+            if (favicon) {
+                favicon.href = `${import.meta.env.BASE_URL}favicon/favicon-admin-ChatGPT.png`;
+            }
+        })        
+        .catch(() => {
+            // Si la session n'est pas valide, rediriger l'utilisateur vers la page de connexion
+            navigate("/league-monteregie/admin");
+        });
+
+    }, [navigate]);
 
     return (
         <div>
+            <div className="admin-navbar">
+                <a href="#" className="admin-navbar-link"
+                    onClick={(e) => { e.preventDefault(); navigate("/league-monteregie/admin/dashboard");}}
+                >
+                    <MdAdminPanelSettings />
+                    <span>Retour à la section admin</span>
+                </a>
 
+                <a href="#" className="admin-navbar-link"
+                    onClick={(e) => { e.preventDefault(); handleLogout("/league-monteregie/");}}
+                >
+                    <FaHouse />
+                    <span>Retour au site principal</span>
+                </a>
+
+                <a href="#" className="admin-navbar-link"
+                    onClick={(e) => {e.preventDefault(); handleLogout("/league-monteregie/admin/");}}
+                >
+                    <MdLogout />
+                    <span>Déconnexion</span>
+                </a>
+            </div>
             <div className="dashboard-container">
 
                 <h1 className="gestion-title">

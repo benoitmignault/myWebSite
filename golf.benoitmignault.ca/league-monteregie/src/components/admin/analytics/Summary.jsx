7b557b37ae284b65aadf422eaea2e572afdc2c24
Benoit Mignault <b.mignault@gmail.com>
2026-07-25 23:15:18 -0400

    ajout des icones d'une librairie

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/Summary.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/Summary.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { LuHouse, LuEye, LuUser, LuCalendarDays, LuChartLine } from "react-icons/lu";
 import { API_BASE_URL } from "../../../config";
 
 

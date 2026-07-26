829f1c0267acf220bca92d1e39b50c0da873bbb4
Benoit Mignault <b.mignault@gmail.com>
2026-07-25 20:23:36 -0400

    création de l'enfant Summary

---
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/Summary.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../../../config";
+
+
+function Summary({ selectedPeriod }) {
+
+
+
+    return (
+
+        <></>
+        
+    );
+
+
+};
+
+export default Summary;

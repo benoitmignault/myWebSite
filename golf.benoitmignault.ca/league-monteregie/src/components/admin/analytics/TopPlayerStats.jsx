60de8729700a36340df3a6994017a6587152b8d4
Benoit Mignault <b.mignault@gmail.com>
2026-08-06 21:50:18 -0400

    feat(admin/analytics): Import `LuUser` icon for player stats

--- golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/TopPlayerStats.jsx
+++ golf.benoitmignault.ca/league-monteregie/src/components/admin/analytics/TopPlayerStats.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { LuLoaderCircle, LuTriangleAlert, LuInfo } from "react-icons/lu";
+import { LuLoaderCircle, LuTriangleAlert, LuInfo, LuUser } from "react-icons/lu";
 import { API_BASE_URL } from "../../../config";
 
 /**
